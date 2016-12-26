b59e6363cd599b1aa7df018c8e01c4583cfb1b59
mcgrady_525 <mcgrady_525@163.com>
2016-12-26 17:31:44 +0800

    ...

--- RabbitMQDemo.Topic.Consumer/Program.cs
+++ RabbitMQDemo.Topic.Consumer/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Threading;
 
 namespace RabbitMQDemo.Topic.Consumer
 {
@@ -9,8 +12,51 @@ namespace RabbitMQDemo.Topic.Consumer
     {
         public static void Main(string[] args)
         {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",//或127.0.0.1
+                Port = 5672,//默认值
+                UserName = "admin",
+                Password = "P@ssw0rd.123"
+            };
 
+            //创建connection和channel
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //创建交换机
+                    channel.ExchangeDeclare(exchange: "logs_topic", type: ExchangeType.Topic, durable: true);
 
+                    //创建queue，服务器生成的随机队列名，后面会自动清除
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    //创建bindings
+                    channel.QueueBind(queue: queueName, exchange: "logs_topic", routingKey: "error");
+
+                    //接收(消费)消息
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var msg = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine("[x] Received RoutingKey:{0},msg:{1}", routingKey, msg);
+
+                        //模拟耗时操作
+                        Thread.Sleep(1000);
+                        Console.WriteLine("[x] Done");
+                    };
+
+                    channel.BasicConsume(queue: queueName,
+                        noAck: true,
+                        consumer: consumer);
+
+                    //这段代码不能放在外面，否则接收不到消息
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
--- RabbitMQDemo.Topic.Producer/Program.cs
+++ RabbitMQDemo.Topic.Producer/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RabbitMQ.Client;
 
 namespace RabbitMQDemo.Topic.Producer
 {
@@ -12,8 +13,43 @@ namespace RabbitMQDemo.Topic.Producer
     {
         public static void Main(string[] args)
         {
+            //
 
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",//或127.0.0.1
+                Port = 5672,//默认值
+                UserName = "admin",
+                Password = "P@ssw0rd.123"
+            };
 
+            //创建connection和channel
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //创建交换机
+                    channel.ExchangeDeclare(exchange: "logs_topic", type: ExchangeType.Topic, durable: true);
+
+                    //生成消息(批量)
+                    for (int i = 0; i < 10; i++)
+                    {
+                        var msg = string.Format("message.{0}", i);
+                        var body = Encoding.UTF8.GetBytes(msg);
+
+                        //发送消息
+                        channel.BasicPublish(exchange: "logs_topic",
+                            routingKey: "error",//对直连交换机有用，对扇形交换机无效
+                            basicProperties: null,
+                            body: body);
+
+                        Console.WriteLine("[x] Sent {0}", msg);
+                    }
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
