664961c59741fe8f3997182463af2e405629717e
mcgrady_525 <mcgrady_525@163.com>
2016-12-26 17:11:57 +0800

    add topic交换机模式。

---
+++ RabbitMQDemo.Topic.Consumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQDemo.Topic.Consumer
+{
+    public class Consumer
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
---
+++ RabbitMQDemo.Topic.Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQDemo.Topic.Producer
+{
+    /// <summary>
+    /// 主题交换机
+    /// </summary>
+    public class Producer
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
